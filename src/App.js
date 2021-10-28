771eed86a004ac88921cfb7909999951eb2c7bb3
ifcodes <ifeco133@gmail.com>
2021-10-28 14:43:26 +0100

    feat: added the zoom-in effect on hover, and the toggle button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function App() {
   const pricingContentMonthly = [
     {
+      id: 0,
       name: "Basic",
       price: "$19.99",
       storage: "500 GB Storage",
@@ -10,6 +12,7 @@ function App() {
       transferable: "Send up to 3 GB"
     },
     {
+      id: 1,
       name: "Professional",
       price: "$24.99",
       storage: "1 TB Storage",
@@ -17,6 +20,7 @@ function App() {
       transferable: "Send up to 10 GB"
     },
     {
+      id: 2,
       name: "Master",
       price: "$39.99",
       storage: "2 TB Storage",
@@ -24,19 +28,34 @@ function App() {
       transferable: "Send up to 20 GB"
     },
   ]
+  const [toggle, setToggle] = useState(false)
+
+  // const hoverChange = 
   return (
-   <div className='theHolder w-3/4 flex my-40 mx-auto justify-center'>
-    {pricingContentMonthly.map((priceContent,index)=> 
-      <div key={`c${index}`} className='w-80 mx-1 bg-white rounded-md p-8 text-center shadow-md'>
-        <h3>{priceContent.name}</h3>
-        <h1 className='font-bold text-5xl'>{priceContent.price}</h1>
-        <p>{priceContent.storage}</p>
-        <p>{priceContent.userAllowed}</p>
-        <p>{priceContent.transferable}</p>
-        <button>LEARN MORE</button>
+    <div>
+      <h1 className='text-gray-800 text-4xl text-center mt-8'>Our Pricing</h1>
+      <div className='w-80 flex justify-between mx-auto mt-4 items-center'>
+        <span className=''>Annually</span>
+        <span onClick={() => setToggle(() => toggle === false ? true : false)} className={`${toggle ? 'justify-end': 'justify-start' } toggle w-10 h-6 rounded-full 
+        justify-start
+        border-2 flex  items-center`}></span>
+        <span>Monthly</span>
       </div>
-    )}
-   </div>
+      <div className='theHolder h-[80%] w-3/4 flex my-20 mx-auto justify-center'>
+
+        {pricingContentMonthly.map((priceContent,index)=> 
+          <div key={`c${index}`} className={`theHoverChange w-80 mx-1 bg-white rounded-md px-8 py-12 text-center shadow-md text-xs transition-all`}>
+            <h3 className='border-b-0 text-sm'>{priceContent.name}</h3>
+            <h1 className='text-5xl py-4 border-b'>{priceContent.price}</h1>
+            <p>{priceContent.storage}</p>
+            <p>{priceContent.userAllowed}</p>
+            <p>{priceContent.transferable}</p>
+            <button className='w-64 border-2 border-solid border-gray-400 my-8 px-4 py-2 rounded text-primary text-xs text-bold '>LEARN MORE</button>
+          </div>
+        )}
+
+      </div>
+    </div>
   );
 }
 
